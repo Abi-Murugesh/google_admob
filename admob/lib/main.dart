6a8993e7f3cf3ee9d71001d35e50e8aef922b610
Abi-Murugesh <abipencilcard@gmail.com>
2024-10-15 16:00:13 +0530

    Create main.dart

---
+++ admob/lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Google Admob',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  BannerAd bAd = new BannerAd(
+      size: AdSize.banner,
+      adUnitId: 'ca-app-pub-3940256099942544/9214589741',
+      listener: BannerAdListener(
+        onAdLoaded: (Ad ad) {
+          print('Ad Loaded');
+        },
+        onAdFailedToLoad: (Ad ad, LoadAdError error) {
+          print('Ad failed to Load');
+          ad.dispose();
+        },
+        onAdOpened: (Ad ad) {
+          print('Ad Opened');
+        },
+      ),
+      request: AdRequest());
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: Container(
+        child: AdWidget(
+          ad: bAd..load(),
+          key: UniqueKey(),
+        ),
+      ),
+    );
+  }
+}
