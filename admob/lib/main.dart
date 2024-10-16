f8c64116990f81a9f7e088623a90223f2d387b88
Abi Murugesh <abipencilcard@gmail.com>
2024-10-16 15:07:33 +0530

    Update main.dart

--- admob/lib/main.dart
+++ admob/lib/main.dart
@@ -29,32 +29,58 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  BannerAd bAd = new BannerAd(
+  BannerAd? _bannerAd;
+  bool _isAdLoaded = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadAd();
+  }
+
+  void _loadAd() {
+    _bannerAd = BannerAd(
       size: AdSize.banner,
       adUnitId: 'ca-app-pub-1277658302369683/6043045696',
       listener: BannerAdListener(
         onAdLoaded: (Ad ad) {
           print('Ad Loaded');
+          setState(() {
+            _isAdLoaded = true;
+          });
         },
         onAdFailedToLoad: (Ad ad, LoadAdError error) {
-          print('Ad failed to Load');
+          print('Ad failed to Load: $error');
           ad.dispose();
         },
         onAdOpened: (Ad ad) {
           print('Ad Opened');
         },
       ),
-      request: AdRequest());
+      request: AdRequest(),
+    );
+
+    _bannerAd?.load();
+  }
+
+  @override
+  void dispose() {
+    _bannerAd?.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: Container(
-        child: AdWidget(
-          ad: bAd..load(),
-          key: UniqueKey(),
-        ),
-        height: 50,
-      ),
+      appBar: AppBar(title: Text('Google AdMob Example')),
+      body: Center(child: Text('Your app content here')),
+      bottomNavigationBar: _isAdLoaded
+          ? Container(
+              height: _bannerAd!.size.height.toDouble(),
+              width: _bannerAd!.size.width.toDouble(),
+              child: AdWidget(ad: _bannerAd!, key: UniqueKey()),
+            )
+          : SizedBox(),
     );
   }
 }
